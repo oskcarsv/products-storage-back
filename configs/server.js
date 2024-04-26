79c50630b3d9a0a5bfa2865c54e93618f927ed6f
oskcarsv <oskcarqsv@gmail.com>
2024-04-26 11:00:08 -0600

    modify user path

--- configs/server.js
+++ configs/server.js
@@ -18,7 +18,7 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-        this.usuarioPath = '/products-storage/v1/users';
+        this.usuarioPath = '/products-storage/v1/user';
         this.authPath = '/products-storage/v1/auth';
         this.taskPath = '/products-storage/v1/task';
 
