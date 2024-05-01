6831a3f80bafe1bb072821b3db5d40c89d29a8ca
oskcarsv <oskcarqsv@gmail.com>
2024-04-30 21:47:04 -0600

    modify login function to accept username and email

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -3,44 +3,80 @@ import User from '../user/user.model.js'
 import { generateJWT } from '../helpers/generate-jwt.js'; 
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
+  const { usernameOrEmail, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+try {
+  
+  if(usernameOrEmail.includes('@')){
 
-    if (!user) {
+      const user = await User.findOne({ email: usernameOrEmail });
+
+      if (!user) {
       return res.status(400).json({
-        msg: "Wrong Credentials, Email doesn't exists in database",
+          msg: "Wrong Credentials, Email doesn't exists in database",
       });
-    }
+      }
 
-    if (!user.state) {
+      if (!user.state) {
       return res.status(400).json({
-        msg: "The User doesn't exists in database",
+          msg: "The User doesn't exists in database",
       });
-    }
+      }
 
-    const validPassword = bcryptjs.compareSync(password, user.password);
-    if (!validPassword) {
+      const validPassword = bcryptjs.compareSync(password, user.password);
+      if (!validPassword) {
       return res.status(400).json({
-        msg: "Wrong Password",
+          msg: "Wrong Password",
       });
-    }
+      }
 
-    const token = await generateJWT( user.id);
+      const token = await generateJWT( user.id);
 
-    res.status(200).json({
+      res.status(200).json({
       msg: 'Login Successful',
       user,
       token
-    });
+      });
 
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      msg: "Comunicate with the support",
-    });
+  }else{
+
+      const user = await User.findOne({ username: usernameOrEmail });
+
+      if (!user) {
+      return res.status(400).json({
+          msg: "Wrong Credentials, Username doesn't exists in database",
+      });
+      }
+
+      if (!user.state) {
+      return res.status(400).json({
+          msg: "The User doesn't exists in database",
+      });
+      }
+
+      const validPassword = bcryptjs.compareSync(password, user.password);
+      if (!validPassword) {
+      return res.status(400).json({
+          msg: "Wrong Password",
+      });
+      }
+
+      const token = await generateJWT( user.id);
+
+      res.status(200).json({
+      msg: 'Login Successful',
+      user,
+      token
+      });
+
   }
+
+} catch (e) {
+  console.log(e);
+  res.status(500).json({
+    msg: "Comunicate with the support",
+  });
+}
 }
 
 export const register = async (req, res) => {
